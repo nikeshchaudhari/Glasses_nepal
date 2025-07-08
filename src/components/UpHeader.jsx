beb8f6167dcadea3c84cb9eaa0d947cbf9c8eeb3
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-08 23:14:11 +0545

    glass nepal

---
+++ src/components/UpHeader.jsx
@@ -0,0 +1,9 @@
+const UpHeader =()=>{
+  const text = "Upto 20% off || Shop Now";
+    return(
+        <div className="upHead text-white bg-black w-full py-4  ">
+          <h4 className="text-center ">{text}</h4>  
+        </div>
+    )
+}
+export default UpHeader;
