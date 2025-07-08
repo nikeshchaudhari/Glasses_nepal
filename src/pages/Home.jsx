40893b350ba2c150ea1d76bbc9ce1d216bade0e9
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-08 23:14:30 +0545

    glass nepal

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UpHeader from '../components/UpHeader'
+import MainHeader from '../components/MainHeader'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <UpHeader/>
+      <MainHeader/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
