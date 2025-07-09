dfa300c77db5690fd56bf48480b98701d99c88b3
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-09 23:52:42 +0545

    Glasses nepal

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import UpHeader from "../components/UpHeader";
+import Navbar from "../components/Navbar";
+import MainHeader from "../components/MainHeader";
+import ProductList from "../components/ProductList";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <MainHeader />
+      <Navbar />
+      <ProductList/>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
