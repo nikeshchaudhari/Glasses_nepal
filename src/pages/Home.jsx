56fe9be00427a1c530d7a0761149a98f0749c456
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-09 23:52:31 +0545

    Glasses nepal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import UpHeader from '../components/UpHeader'
 import MainHeader from '../components/MainHeader'
 import Navbar from '../components/Navbar'
+import Carasole from '../components/Carasole'
+import SearchInput from '../components/SearchInput'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <UpHeader/>
       <MainHeader/>
       <Navbar/>
+      <Carasole/>
+    
     </div>
   )
 }
