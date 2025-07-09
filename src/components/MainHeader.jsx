762bcc579b639a51f1e0c49492a253835d25f8d9
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-09 23:51:14 +0545

    Glasses nepal

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 
 const MainHeader = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const showHandle = () => {
+    setShowSearch((e) => !e);
+    console.log("show");
+  };
   return (
-    <div className='flex justify-between items-center w-full pt-10 pb-5 px-6 sm:px-20 flex-wrap shadow sticky'>
-      <div className='cursor-pointer text-2xl'><FaSearch /></div>
-      <div className='font-bold text-3xl'>Glasses Nepal</div>
-      <div className='cursor-pointer text-2xl'><IoCartOutline /></div>
-    
+    <div className="flex justify-between items-center w-full pt-10 pb-5 px-6 sm:px-20 flex-wrap shadow sticky">
+      <div className="cursor-pointer text-2xl" onClick={showHandle}>
+        <FaSearch />
+      </div>
+      <div className="font-bold text-3xl">Glasses Nepal</div>
+      <div className="cursor-pointer text-2xl">
+        <IoCartOutline />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainHeader;
