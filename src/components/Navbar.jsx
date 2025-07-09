45343f0dc6d4561ac843f94a0b1124a7ba996e06
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-09 22:16:37 +0545

    Glasses nepal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Carasole from "./Carasole";
 
 const Navbar = () => {
   return (
-    <div className='bg-amber-200'>
-      <ul className='flex justify-center gap-10 mt-3 py-3 items-center'>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/product">All Product</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
-        <li><Link to="/about-us">About</Link></li>
-      </ul>
+    <div className="">
+      <div className=" shadow-xl w-full ">
+        <ul className=" hidden md:flex justify-center gap-10 mt-3 py-3 items-center sticky ">
+          <li>
+            <Link
+              to="/home"
+              className="hover:font-bold transition-all duration-2000"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/product"
+              className="hover:font-bold transition-all duration-2000"
+            >
+              All Product
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/categories"
+              className="hover:font-bold transition-all duration-2000"
+            >
+              Categories
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about-us"
+              className="hover:font-bold transition-all duration-2000"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
