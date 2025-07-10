84a6ef388462ad71bb98eb6c0415bcabc604879a
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-10 23:26:04 +0545

    glass nepal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Carasole from "./Carasole";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoIosArrowUp } from "react-icons/io";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="">
-      <div className=" shadow-xl w-full  ">
-        <ul className=" hidden md:flex justify-center gap-10 mt-3 py-3 items-center  ">
+      <div className="shadow-xl w-full">
+        <ul className="hidden md:flex justify-center gap-10 mt-3 py-3 items-center">
           <li>
-            <Link
-              to="/home"
-              className="hover:font-bold transition-all duration-2000"
-            >
+            <Link to="/home" className="hover:font-bold transition-all">
               Home
             </Link>
           </li>
           <li>
-            <Link
-              to="/product"
-              className="hover:font-bold transition-all duration-2000"
-            >
+            <Link to="/product" className="hover:font-bold transition-all">
               All Product
             </Link>
           </li>
-          <li>
-            <Link
-              to="/categories"
-              className="hover:font-bold transition-all duration-2000"
-            >
+
+          <li
+            className="relative"
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+          >
+            <div className="flex items-center cursor-pointer hover:font-bold transition-all">
               Categories
-            </Link>
+              {isOpen ? (
+                <IoIosArrowUp className="inline ml-1" />
+              ) : (
+                <MdKeyboardArrowDown className="inline ml-1" />
+              )}
+            </div>
+            {/* Dropdown Category */}
+            {isOpen && (
+              <ul className="absolute top-full left-0 w-40 bg-white p-2 rounded-b-sm shadow-md z-50">
+                <li className="mb-2 hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
+                  Mens
+                </li>
+                <li className="mb-2 hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
+                  Women
+                </li>
+                <li className="mb-2 hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
+                  Sunglasses
+                </li>
+                <li className="mb-2 hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
+                  Sportglasses
+                </li>
+              </ul>
+            )}
           </li>
+
           <li>
-            <Link
-              to="/about-us"
-              className="hover:font-bold transition-all duration-2000"
-            >
+            <Link to="/about-us" className="hover:font-bold transition-all">
               About
             </Link>
           </li>
