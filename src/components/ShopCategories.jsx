5e9bfcba3016577df8ecdff8c94c89e10f1ae695
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-13 00:43:42 +0545

    glass nepal

---
+++ src/components/ShopCategories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ShopCategories = () => {
+  return (
+    <div className='bg-[#e8e8e8]'>
+<h1 className=' text-center p-10 text-[30px] font-bold'>SHOP BY CATEGORY</h1>
+<div>
+<div className="">
+    
+</div>
+</div>
+
+    </div>
+  )
+}
+
+export default ShopCategories
