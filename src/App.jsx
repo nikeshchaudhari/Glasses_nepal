5f164ffc1ae04b13b7473fd6f4e2d25d7b5bc881
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-15 23:50:59 +0545

    glasses Nepal

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
+import Mens from "./categories/Mens";
 
 const myroute = createBrowserRouter([
   {path:"",Component:Home},
   {path:"/home",Component:Home},
   {path:"/product",Component:Products},
+  {path:"/categories/men",Component:Mens},
   {path:"*",Component:Error}
 ])
 const App = () => {
