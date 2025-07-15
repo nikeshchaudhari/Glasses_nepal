a7b7695eb130051af687d47e139e900aa8f113d1
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-15 16:26:00 +0545

    glasses Nep[al

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-
-        <div className="w-[400px] ">
-          <h5 className="mb-5">FILTER BY :</h5>
-        <hr className="mb-5"/>
+    <div className="w-[400px] hidden md:block ">
+      <h5 className="mb-5 ">FILTER BY :</h5>
+      <hr className="mb-5 opacity-15" />
       <div>
         <h5>Price</h5>
-        <div className='flex justify-between'>
-          <h5>From</h5> 
+        <div className="flex justify-between w-[180px] mt-8">
+          <h5>From</h5>
           <h5>To </h5>
         </div>
-        <div className="flex w-5">
-          <input type="number" className="w-15"/>
-          <span className="">-</span>
-          <input type="number" name="" id="" className="w-15" />
+        <div className="flex  items-center">
+          <input type="number" min="0" className="w-30 border-1 px-2 py-1" />
+          <span className=" mx-4 flx items-center">-</span>
+          <input
+            type="number"
+            min="5000"
+            name=""
+            id=""
+            className="w-30 border-1 px-2 py-1"
+          />
+        </div>
+        <hr className="mt-10 opacity-15" />
+        <div className="mt-5">
+          <h5>Category</h5>
+        <input type="checkbox" name="men" id="men" className="mx-5 mt-5 "/>
+          <label htmlFor="men">Men</label>
         </div>
       </div>
-      
+    </div>
+  );
+};
 
-        </div>  )
-}
-
-export default Sidebar
+export default Sidebar;
