ed1681f1a234d392ec3ef50dd17595140e0c27d8
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-15 23:33:13 +0545

    glasses Nepal

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -15,7 +15,7 @@ const MainHeader = () => {
           <img
             src="https://cdn2.blanxer.com/brand_logo/63e45741f1328721d34d41e8/660043f51786c9bc24ea2fb0.webp"
             alt="image"
-            className="w-[250px] "
+            className="md:w-[250px] w-[150px] "
           />
         </div>
         <div className="flex gap-5">
@@ -32,6 +32,13 @@ const MainHeader = () => {
             <div onClick={()=>setIsOpen(false)} className="fixed top-0 right-0 w-[300px] h-full  bg-white shadow-lg z-40 transform transition duration-1000  animate-slide">
           <div className="w-full flex justify-end px-5 py-5">
              <CloseOutlinedIcon onClick={()=>setIsOpen(false)} className="text-8xl"/>
+          </div>
+          <div className="mx-5">
+            <ul >
+              <li className="mb-2">HOME</li>
+              <li className="mb-2">All Product</li>
+              <li className="mb-2">Categories</li>
+            </ul>
           </div>
             </div>
           </>
