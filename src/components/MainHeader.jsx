a9e28c859ce0dc3a0bede24c6f43d8a2bc1e1a2d
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-16 22:53:17 +0545

    Glasses Nepal

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 
 const MainHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isArrow, setIsArrow] = useState(false);
   return (
     <>
       <div className="flex justify-between items-center px-4  md:px-15 py-5 ">
@@ -31,20 +32,46 @@ const MainHeader = () => {
           <>
             <div onClick={() => setIsOpen(false)} className="bg-black"></div>
             {/* Drawer */}
-            <div onClick={()=>setIsOpen(false)} className="fixed top-0 right-0 w-[300px] h-full  bg-white shadow-lg z-40 transform transition duration-1000  animate-slide">
-          <div className="w-full flex justify-end px-5 pt-5">
-             <CloseOutlinedIcon onClick={()=>setIsOpen(false)} className="text-8xl"/>
-          </div>
-          <div className="mx-5">
-            <div className="flex justify-center mb-5">
-              <img src="https://cdn2.blanxer.com/brand_logo/63e45741f1328721d34d41e8/660043f51786c9bc24ea2fb0.webp" alt="" className="w-[200px]"/>
-            </div>
-            <ul >
-              <li className="mb-2"><Link to="/home">HOME</Link></li>
-              <li className="mb-2">All Product</li>
-              <li className="mb-2">Categories</li>
-            </ul>
-          </div>
+            <div
+              // onClick={() => setIsOpen(false)}
+              className="fixed top-0 right-0 w-[300px] h-full  bg-white shadow-lg z-40 transform transition duration-1000  animate-slide"
+            >
+              <div className="w-full flex justify-end px-5 pt-5">
+                <CloseOutlinedIcon
+                  onClick={() => setIsOpen(false)}
+                  className="text-8xl"
+                />
+              </div>
+              <div className="mx-5">
+                <div className="flex justify-center mb-5">
+                  <img
+                    src="https://cdn2.blanxer.com/brand_logo/63e45741f1328721d34d41e8/660043f51786c9bc24ea2fb0.webp"
+                    alt=""
+                    className="w-[200px]"
+                  />
+                </div>
+                <ul>
+                  <li className="mb-2">
+                    <Link to="/home">HOME</Link>
+                  </li>
+                  <li className="mb-2">
+                    <Link to="/product">All Product</Link>
+                  </li>
+                  <li className="mb-2" onClick={() => setIsArrow(true)}>
+                    Categories
+                    {isArrow && (
+                      <div className="mx-5 mt-2">
+                        <ul>
+                          <li className="mb-1">Mens</li>
+                          <li className="mb-1">Womens</li>
+                          <li className="mb-1">Sunglasses</li>
+                          <li className="mb-1">Sport Glasses</li>
+                        </ul>
+                      </div>
+                    )}
+                  </li>
+                </ul>
+              </div>
             </div>
           </>
         )}
