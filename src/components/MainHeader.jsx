45df4a046fc68239be16ba5db95b22c7f068dbf6
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-16 22:21:28 +0545

    Glasses Nepal

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -32,10 +32,13 @@ const MainHeader = () => {
             <div onClick={() => setIsOpen(false)} className="bg-black"></div>
             {/* Drawer */}
             <div onClick={()=>setIsOpen(false)} className="fixed top-0 right-0 w-[300px] h-full  bg-white shadow-lg z-40 transform transition duration-1000  animate-slide">
-          <div className="w-full flex justify-end px-5 py-5">
+          <div className="w-full flex justify-end px-5 pt-5">
              <CloseOutlinedIcon onClick={()=>setIsOpen(false)} className="text-8xl"/>
           </div>
           <div className="mx-5">
+            <div className="flex justify-center mb-5">
+              <img src="https://cdn2.blanxer.com/brand_logo/63e45741f1328721d34d41e8/660043f51786c9bc24ea2fb0.webp" alt="" className="w-[200px]"/>
+            </div>
             <ul >
               <li className="mb-2"><Link to="/home">HOME</Link></li>
               <li className="mb-2">All Product</li>
