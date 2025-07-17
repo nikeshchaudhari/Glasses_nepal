16e994b945528f1bf2c53bdb7a72e92d6dff1e4c
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-18 00:07:58 +0545

    glasses Nepal

--- src/components/ShopCategories.jsx
+++ src/components/ShopCategories.jsx
@@ -4,24 +4,28 @@ import { Link } from "react-router-dom";
 const image = [
   {
     id: 0,
+    name:"Mens",
     image:
       "https://images.pexels.com/photos/19719795/pexels-photo-19719795.jpeg",
     link: "/categories/men",
   },
   {
     id: 1,
+    name:"Womens",
     image:
       "https://t4.ftcdn.net/jpg/05/30/66/13/360_F_530661385_Suo3ujSOX6nTDDc6QWYLZrbz7Dk0nW1W.jpg",
     link: "/categories/women",
   },
   {
     id: 2,
+    name:"Sunglasses",
     image:
       "https://images.pexels.com/photos/29538710/pexels-photo-29538710.jpeg",
     link: "/categories/sunglass",
   },
   {
     id: 3,
+    name:"Sportglasses",
     image: "https://images.pexels.com/photos/5807795/pexels-photo-5807795.jpeg",
     link: "/categories/sportglass",
   },
@@ -35,13 +39,17 @@ const ShopCategories = () => {
       </h1>
       <div className="w-full grid gap-3 grid-cols-2 lg:grid-cols-4 px-10 ">
         {image.map((items, index) => (
-          <div key={index} className="bg-slate-950 rounded-xl">
+          <div key={index} className=" rounded-xl">
             <Link to={items.link}>
+            <div className="relative  w-full h-[150px] lg:w-[300px]">
+              <h2 className=" hidden lg:block absolute lg:top-40 lg:left-25 lg:bg-black bg-black lg:p-2 lg:opacity-60 lg:rounded-br-4xl  top-16 text-[10px] lg:text-[20px] text-white font-extrabold ">{items.name}</h2>
+           
               <img
                 src={items.image}
                 alt={items.id}
-                className="bg-slate-400 opacity-60 rounded-xl w-full h-[150px] lg:h-[400px] hover:opacity-100 transition duration-700 cursor-pointer object-cover"
+                className=" opacity-100 rounded-xl w-full h-[150px] lg:h-[400px] transform transition hover:transform-y-20  "
               />
+               </div>
             </Link>
           </div>
         ))}
