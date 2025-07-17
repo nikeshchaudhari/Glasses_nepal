beb827aeaf71bd4643796bab95f38ce33eb7d771
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-17 22:53:23 +0545

    Glasses Nepal

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import Mens from "./categories/Mens";
+import About from "./pages/About";
 
 const myroute = createBrowserRouter([
   {path:"",Component:Home},
   {path:"/home",Component:Home},
   {path:"/product",Component:Products},
   {path:"/categories/men",Component:Mens},
+  {path:"/about-us",Component:About},
   {path:"*",Component:Error}
 ])
 const App = () => {
