666a9fdf6a8145607a821cdffa55295bc579c958
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-18 21:01:46 +0545

    glasses Nepal

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,19 +4,27 @@ import Sidebar from "./Sidebar";
 
 const ProductList = () => {
   const [items, setItems] = useState([]);
+  const [minPrice,setMinPrice]= useState("")
+  const [maxPrice,setMaxPrice]= useState("")
 
   useEffect(() => {
     const dataFetch = async () => {
       try {
         const res = await axios.get("https://dummyjson.com/products");
         setItems(res.data.products);
-        console.log(res);
+        console.log(res.data);
       } catch (err) {
         console.error("Error fetching data:", err);
       }
     };
     dataFetch();
   }, []);
+  const filterItems = minPrice === "" && maxPrice ==="" ? items : items.filter(item=>{
+    const price = item.price;
+    const min = minPrice ===""?0:parseInt(minPrice)
+    const max = maxPrice ===""?Infinity:parseInt(maxPrice)
+    return price >= min && price <=max 
+  })
 
   return (
     <div className="min-h-screen overflow-x-hidden">
@@ -24,10 +32,11 @@ const ProductList = () => {
         ALL PRODUCTS
       </h3>
       <div className="flex h-min-screen  px-10">
-        <Sidebar />
+        <Sidebar setMinPrice={setMinPrice} setMaxPrice={setMaxPrice}/>
         <div className="flex justify-center flex-wrap w-full px-4 bg-white p-4 mt-5">
-          {items.map((item, index) => (
-            <div
+          {filterItems.length > 0 ?(
+            filterItems.map((item,index)=>(
+                <div
               key={index}
               className="w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4"
             >
@@ -56,7 +65,12 @@ const ProductList = () => {
                 </div>
               </div>
             </div>
-          ))}
+
+            ))
+          ):(
+            <p>NO product Found </p>
+          )}
+          
         </div>
       </div>
     </div>
