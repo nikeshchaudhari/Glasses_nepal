9058047c1819428a46d3a46916f003d0963d89bb
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-18 21:20:00 +0545

    glasses Nepal

--- src/categories/Mens.jsx
+++ src/categories/Mens.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Mens = () => {
+  const [men, setMen] = useState([]);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await axios.get("https://dummyjson.com/products");
+        console.log(res.data.products);
+        const menProduct =  res.data.products.filter(item=>{
+          item.category.include("beauty")
+        })
+
+        setMen(menProduct);
+      } catch (err) {
+        console.log("Something Wrong", err);
+      }
+    };
+    fetchProduct();
+  }, []);
   return (
+    <>
     <div>
-      hello
+      
     </div>
+    </>
   )
 }
 
-export default Mens
+export default Mens;
