923ad42030c3e7a2ca7ac5bac7ff04dca27dc479
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-19 23:47:43 +0545

    glasses Nepal

--- src/categories/Mens.jsx
+++ src/categories/Mens.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar"
-import MainHeader from "../components/MainHeader"
+import Navbar from "../components/Navbar";
+import MainHeader from "../components/MainHeader";
 const Mens = () => {
   const [men, setMen] = useState([]);
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const res = await axios.get("https://dummyjson.com/products/category/beauty");
+        const res = await axios.get(
+          "https://dummyjson.com/products/category/beauty"
+        );
         console.log(res.data.products);
         // const menProduct =  res.data.products.filter(item=>{
         //   item.category.include("beauty")
@@ -22,25 +24,33 @@ const Mens = () => {
   }, []);
   return (
     <>
-    <MainHeader/>
-    <Navbar/>
-    <div>
-      <h3 className="text-center mt-10 text-[30px] font-extrabold">Mens</h3>
-      <div>
-        {men.length > 0 ?(
-men.map((item,index)=>(
-  <div key={index}>
-    <img src={item.images} alt="" />
-  </div>
-))
+      <MainHeader />
+      <Navbar />
+      <div className="min-h-screen  overflow-x-hidden ">
+        <h3 className="flex justify-center mt-5 py-5 font-bold text-[24px] mb-5">
+          Mens
+        </h3>
+        <div className="flex p-10 justify-start flex-wrap">
+          {men.length > 0 ? (
+            men.map((item, index) => (
+              <div key={index} className="w-1/2 md:w-1/3 lg:w-1/5">
+                <div  className="bg-white shadow-2xl m-2 rounded-2xl">
+                <div className="">
+                <img src={item.images} alt="" />
+
+                </div>
+                <h3 className="font-semibold text-center">{item.title}</h3>
+              </div>
+              </div>
+            ))
+          ) : (
+            <p>Not Found</p>
+          )}
           
-        ):(
-          <p>Not Found</p>
-        )}
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default Mens;
