e612fa943fc59fec0c342a3f795fb66ff8efe4a3
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-20 23:40:44 +0545

    glasses Nepal

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Women from "./categories/Women";
 import About from "./pages/About";
 import Sunglass from "./categories/Sunglass";
 import SportGlass from "./categories/SportGlass"
+import ProductDetails from "./components/ProductDetails";
 
 
 const myroute = createBrowserRouter([
   {path:"",Component:Home},
   {path:"/home",Component:Home},
   {path:"/product",Component:Products},
+  {path:"/product/:id",Component:ProductDetails},
   {path:"/categories/men",Component:Mens},
   {path:"/categories/women",Component:Women},
   {path:"/categories/sunglass",Component:Sunglass},
