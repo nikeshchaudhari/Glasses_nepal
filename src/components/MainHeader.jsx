b6853935a76f800fdfda21818c3f887be76e8e5d
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-21 13:57:53 +0545

    glasses Nepal

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -6,32 +6,48 @@ import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
 import { Link } from "react-router-dom";
-
+import SearchInput from "./SearchInput";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 const MainHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isArrow, setIsArrow] = useState(false);
-  const [cartCount,setCartCount]= useState(0)
-useEffect(()=>{
-  const cart = JSON.parse(localStorage.getItem("cart"))|| []
-  setCartCount(cart.length)
+  const [cartCount, setCartCount] = useState(0);
+  const [showSearch, setShowSearch] = useState(false);
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartCount(cart.length);
 
-  const handlecartUpdate = ()=>{
-    const updateCart = JSON.parse(localStorage.getItem("cart"))||[]
-    setCartCount(updateCart.length)
-  };
+    const handlecartUpdate = () => {
+      const updateCart = JSON.parse(localStorage.getItem("cart")) || [];
+      setCartCount(updateCart.length);
+    };
 
-  window.addEventListener("cart",handlecartUpdate)
+    window.addEventListener("cart", handlecartUpdate);
 
-  return ()=>{
-    window.removeEventListener("cart",handlecartUpdate)
-  }
-},[])
+    return () => {
+      window.removeEventListener("cart", handlecartUpdate);
+    };
+  }, []);
   return (
     <>
       <div className="flex justify-between items-center px-4  md:px-15 py-5 bg-white sticky z-50 top-0 transition animation-fade">
         <div>
-          <SearchIcon className="flex items-center cursor-pointer" />
+          <SearchIcon
+            onClick={() => setShowSearch(!showSearch)}
+            className="flex items-center cursor-pointer"
+          />
         </div>
+        {showSearch && (
+          <div className="fixed top-0 left-0 w-full bg-white shadow-md  md:p-1 z-50 flex flex-col items-center transition duration-1000 ">
+            <div className="flex items-center gap-3 mt-10">
+              <SearchInput className="mt-5" />
+
+              <div className="mb-12 cursor-pointer" onClick={()=>setShowSearch(false)}>
+                <CloseOutlinedIcon/>
+              </div>
+            </div>
+          </div>
+        )}
         <div>
           <img
             src="https://cdn2.blanxer.com/brand_logo/63e45741f1328721d34d41e8/660043f51786c9bc24ea2fb0.webp"
@@ -42,9 +58,7 @@ useEffect(()=>{
         <div className="flex gap-5 relative">
           <LocalMallOutlinedIcon className="cursor-pointer" />
           {cartCount > 0 && (
-            <span className="absolute -top-2 left-5">
-              {cartCount}
-            </span>
+            <span className="absolute -top-2 left-5">{cartCount}</span>
           )}
           <div className="md:hidden block">
             <MenuOutlinedIcon onClick={() => setIsOpen(true)} />
@@ -88,16 +102,18 @@ useEffect(()=>{
                     <div className="flex justify-between mb-2">
                       Categories
                       {isArrow ? (
-                       <ExpandMoreRoundedIcon />
+                        <ExpandMoreRoundedIcon />
                       ) : (
-                         <ChevronRightRoundedIcon />
+                        <ChevronRightRoundedIcon />
                       )}
                     </div>
 
                     {isArrow && (
                       <div className="mx-5 mt-2 mb-2">
                         <ul>
-                          <li className="mb-1 text-[15px]"><Link to="/categories/men">Mens</Link></li>
+                          <li className="mb-1 text-[15px]">
+                            <Link to="/categories/men">Mens</Link>
+                          </li>
                           <li className="mb-1 text-[15px]">Womens</li>
                           <li className="mb-1 text-[15px]">Sunglasses</li>
                           <li className="mb-1 text-[15px]">Sport Glasses</li>
@@ -105,7 +121,9 @@ useEffect(()=>{
                       </div>
                     )}
                   </li>
-                  <li className="mb-2"><Link to="/about-us">About Us</Link></li>
+                  <li className="mb-2">
+                    <Link to="/about-us">About Us</Link>
+                  </li>
                 </ul>
               </div>
             </div>
