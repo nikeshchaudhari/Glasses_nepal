0d6e7920084899d7478b73e9ac40a79d64330ca8
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-21 15:27:20 +0545

    glasses Nepal

--- src/categories/Women.jsx
+++ src/categories/Women.jsx
@@ -2,16 +2,17 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import MainHeader from "../components/MainHeader";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
 
 const Women = () => {
-  const [women, setWomen] = useState([]);
-
+  const [women, setWomen] = useState([])
+  const navigate = useNavigate()
   const fetchData = async () => {
     try {
       const res = await axios(
         "https://dummyjson.com/products/category/groceries  "
       );
-      console.log(res.data.products);
+      // console.log(res.data.products);
 
       setWomen(res.data.products);
     } catch (err) {
@@ -31,20 +32,23 @@ const Women = () => {
         <div className="flex flex-wrap p-10">
           {women.length > 0 ? (
             women.map((item, index) => (
-              <div key={index} className="w-1/2 md:w-1/3 lg:w-1/5">
-               <div className="bg-white shadow-2xl rounded-2xl m-2 cursor-pointer">
-                 <img
-                  src={
-                    item.images && item.images.length > 0 ? item.images[0] : ""
-                  }
-                  alt=""
-                  className=""
-                />
-               
-               <div>
-                <h3>{item.title}</h3>
-               </div>
-               </div>
+              <div key={index} className="w-1/2 md:w-1/3 lg:w-1/5" onClick={()=>navigate(`/product/${item.id}`)}>
+                <div className="bg-white shadow-2xl rounded-2xl m-2 cursor-pointer transform transition hover:-translate-y-2 hover:duration-500">
+                  <img
+                    src={
+                     
+                        item.images[0]
+                      
+                    }
+                    alt=""
+                    className=""
+                  />
+
+                  <div className="mx-5">
+                    <h3 className="md:font-semibold text-center">{item.title}</h3>
+                    <h3 className="p-5">Rs. {item.price}</h3>
+                  </div>
+                </div>
               </div>
             ))
           ) : (
