2d656f2c429d65df9eb33fd59971ee9d98c5e594
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-24 15:20:59 +0545

    glasses nepal

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { TbTruckDelivery } from "react-icons/tb";
+import { FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
 
 const Cart = ({ cartOpen, cartClose }) => {
   const [cartItem, setCartItem] = useState([]);
@@ -15,6 +17,15 @@ const Cart = ({ cartOpen, cartClose }) => {
     window.addEventListener("cart", handleCart);
     return () => window.removeEventListener("cart", handleCart);
   }, []);
+
+  const quantityUpdate = (id,change)=>{
+    const update = cartItem.map((item)=>{
+      item.id === id ? {...item, quantity:Math.max(1,item.quantity+change)}:item
+    })
+    setCartItem(update)
+    localStorage.setItem("cart",JSON.stringify(update))
+    window.dispatchEvent(new Event("cart"))
+  }
   return (
     <>
       <div
@@ -46,7 +57,22 @@ const Cart = ({ cartOpen, cartClose }) => {
                 <div className=" mx-3 border rounded-lg">
                   <img src={item.images} alt="" className="w-20 " />
                 </div>
-                <h3 className="md:text-[15px] text-[12px] mt-3">{item.title}</h3>
+                <div>
+                  <h3 className="md:text-[15px] text-[12px] mt-3">
+                    {item.title}
+                  </h3>
+
+                  <div className="flex gap-5 mt-5 ml-4 w-full items-center  ">
+                    <div className="bg-black/35 text-white cursor-pointer p-3">
+                      <FaMinus />
+                    </div>
+                    <span></span>
+                    <div className="bg-black/35 text-white cursor-pointer p-3">
+                      <FaPlus />
+                    </div>
+                    <p className="ml-30">Price : {item.price}</p>
+                  </div>
+                </div>
               </div>
             ))
           )}
