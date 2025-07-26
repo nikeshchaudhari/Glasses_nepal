1dc195e6191dcb410fc486c41103495462f70d09
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-26 23:23:41 +0545

    backend

--- glassbackend/Routes/users.js
+++ glassbackend/Routes/users.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 route.post("/add-user", async (req, res) => {
   try {
+    // email check
+    const user = await User.find({email:req.body.email})
+    console.log(user);
+    
+    if(user.length >0){
+        return res.status(500).json({
+            msg:"Email already register... "
+        })
+    }
+    // hash 
     const hash = await bcrypt.hash(req.body.password,10)
     const addUser = await new User({
         _id : new mongoose.Types.ObjectId(),
@@ -12,6 +22,7 @@ route.post("/add-user", async (req, res) => {
       email: req.body.email,
       password:hash,
     });
+    // user Add & Save
     const userData = await addUser.save();
     res.status(200).json({
      message :"Data add sucessfully",
@@ -26,4 +37,24 @@ route.post("/add-user", async (req, res) => {
   }
 });
 
+// User View or Get
+route.post("/login",async(req,res)=>{
+    try{
+        // find email
+const user = User.find({email:req.body.email})
+if(user === 0){
+    return res.status(401).json({
+        msg:"Email not register..."
+    })
+}
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({
+            error:err
+        })
+        
+    }
+})
 module.exports = route;
