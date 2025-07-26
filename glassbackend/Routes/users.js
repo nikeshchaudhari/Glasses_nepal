1d9083dc4b16c07d4c8cd5d8e43e00681f9b635d
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-26 22:50:59 +0545

    backend

--- glassbackend/Routes/users.js
+++ glassbackend/Routes/users.js
@@ -1,13 +1,27 @@
-const express = require("express")
+const express = require("express");
 const route = express.Router();
-
-route.post("/add-user",async(req,res)=>{
-    try{
-
-    }
-    catch(err){
-        
-    }
-})
+const User = require("../Module/Users");
+const mongoose = require("mongoose")
+route.post("/add-user", async (req, res) => {
+  try {
+    const addUser = await new User({
+        _id : new mongoose.Types.ObjectId(),
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const userData = await addUser.save();
+    res.status(200).json({
+     message :"Data add sucessfully",
+     fullName:userData.fullName,
+     email:userData.email
+    });
+  } catch (err) {
+    console.log("Something Wrong");
+    res.status(400).json({
+      error: err,
+    });
+  }
+});
 
 module.exports = route;
