72cd6d2dc4b2401326387548d651e20b8c122603
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-26 22:58:55 +0545

    backend

--- glassbackend/Routes/users.js
+++ glassbackend/Routes/users.js
@@ -2,13 +2,15 @@ const express = require("express");
 const route = express.Router();
 const User = require("../Module/Users");
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 route.post("/add-user", async (req, res) => {
   try {
+    const hash = await bcrypt.hash(req.body.password,10)
     const addUser = await new User({
         _id : new mongoose.Types.ObjectId(),
       fullName: req.body.fullName,
       email: req.body.email,
-      password: req.body.password,
+      password:hash,
     });
     const userData = await addUser.save();
     res.status(200).json({
