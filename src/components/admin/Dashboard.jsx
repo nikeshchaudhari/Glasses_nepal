c7c0b9265b5f824cd36fcf4b3e750428028da047
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-31 23:44:57 +0545

    admin panel

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,35 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import SideNav from './SideNav'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-   <>
-   <div>
-    <div className="md:w-70 w-50 bg-slate-900 h-screen shadow-l text-white hidden md:block">
-<h2 className='md:font-bold md:text-[20px] text-[15px] p-5 text-center'>ADMIN</h2>
-<hr className='opacity-10' />
-<div className=' mt-5 w-full'>
- <ul className='md:text-[20px] text-[15px]'>
-  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
-    <Link className='ml-5'>Dashboard</Link>
-  </li>
-  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
-    <Link className='ml-5'>Orders</Link>
-  </li>
-  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
-    <Link className='ml-5'>Products</Link>
-  </li>
-  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
-    <Link className='ml-5'>Users</Link>
-  </li>
- </ul>
- <button className=" md:text-[20px] text-[15px]  hover:bg-slate-700 py-2 transition hover:duration-500 w-full text-left hover:text-red-700 ">
-  <Link className='ml-5'>Logout</Link>
- </button>
-</div>
+    <>
+    <div className="dashboard-wrapper ">
+        <div className="dashboard-container">
+            <SideNav/>
+        </div>
     </div>
-   </div>
-   </>
+    <Outlet/>
+    </>
   )
 }
 
