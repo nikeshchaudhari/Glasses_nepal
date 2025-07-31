f43bb76ee135996109f43119be8ef5171b289bf9
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-31 22:45:07 +0545

    admin panel

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
    <>
-   Dashboard
+   <div>
+    <div className="w-70 bg-slate-900 h-screen shadow-l text-white">
+<h2 className='font-bold text-[20px] p-5 text-center'>ADMIN</h2>
+<hr className='opacity-10' />
+<div className=' mt-5 w-full'>
+ <ul className='text-[20px]'>
+  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
+    <Link className='ml-5'>Dashboard</Link>
+  </li>
+  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
+    <Link className='ml-5'>Orders</Link>
+  </li>
+  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
+    <Link className='ml-5'>Products</Link>
+  </li>
+  <li className=' hover:bg-slate-700 py-2 transition hover:duration-500'>
+    <Link className='ml-5'>Users</Link>
+  </li>
+ </ul>
+ <button className=" text-[20px]   hover:bg-slate-700 py-2 transition hover:duration-500 w-full text-left ">
+  <Link className='ml-5'>Logout</Link>
+ </button>
+</div>
+    </div>
+   </div>
    </>
   )
 }
