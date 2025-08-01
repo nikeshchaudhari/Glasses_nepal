d8b6e713343d2dca909616bec3e054851f11dba7
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-01 22:56:36 +0545

    admin panel

--- src/components/admin/MainHome.jsx
+++ src/components/admin/MainHome.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import img from "../../assets/5.jpg";
 import { Link } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
 
 const MainHome = () => {
   return (
     <>
-      <div className="w-[79vw] bg-slate-700  h-20 overflow-x-hidden">
-        <div className="flex justify-between items-centers w-full   ">
-          <div className="ml-5 mt-5 text-white text-[25px] font-semibold"><Link to="/dashboard/main-home">Glasses Nepal</Link></div>
-          <div className="flex justify-end items-center gap-5 text-white cursor-pointer">
+      <div className="w-[79vw] bg-slate-700  h-12 overflow-x-hidden">
+        <div className="flex justify-between items-center  ">
+          <div className=" text-white text-[25px] font-semibold ml-5">
+            <Link to="/dashboard/main-home">Glasses Nepal</Link>
+          </div>
+          <div className="flex justify-end items-center  text-white cursor-pointer -40">
             <IoIosNotificationsOutline className="text-[30px]" />
-            <img src={img} alt="" className="w-[10%] rounded-[50%]" />
+            <img src={img} alt="" className="w-[5%] rounded-[50%] mt-2 ml-3" />
+            <span className="mx-3">Nikesh</span>
           </div>
         </div>
+      </div>
+
+      <div className="mt-20">
+        <div className="w-60 ml-5 bg-white shadow-2xl cursor-pointer h-40 flex justify-center flex-col items-center">
+          <FaRegUser  className="text-[80px] text-center"/>
+
+          <h4 className="text-[20px] mt-3" >WELCOME</h4>
+        </div>
+        <div></div>
+        <div></div>
       </div>
     </>
   );
