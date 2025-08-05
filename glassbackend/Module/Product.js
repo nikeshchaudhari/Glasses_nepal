d5e29866b6142c11d6f68ea010604d9c43e247c8
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-05 23:28:12 +0545

    Backend

---
+++ glassbackend/Module/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require ("mongoose")
+const productSchema = new mongoose.Schema({
+    name:{type:String,required:true,unique:true},
+    price:{type:Number,required:true},
+    category:{
+        type:mongoose.Types.ObjectId,
+        ref:"Category",
+        required:true
+    },
+    description:{type:String,required:true},
+    image:{type:String},
+    
+},{timestamps:true});
+
+module.exports = mongoose.model("product",productSchema)
