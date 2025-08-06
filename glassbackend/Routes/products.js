0c8b62c0a0eaf089015123e0a50eb71d7376ea8f
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-06 21:38:50 +0545

    backend

--- glassbackend/Routes/products.js
+++ glassbackend/Routes/products.js
@@ -1,19 +1,26 @@
-const express = require("express")
-const route = express.Router()
-const mongoose = require("mongoose")
-const Product = require("../Module/Product")
-const cloudinary = require("cloudinary").v2
-require("dotenv").config()
+const express = require("express");
+const route = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../Module/Product");
+const cloudinary = require("cloudinary").v2;
+const Auth = require("../middleware/Auth");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 cloudinary.config({
-    cloud_name :process.env.CLOUD_NAME,
-    api_key :process.env.API_KEY,
-    api_secret:process.env.SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.SECRET,
 });
 
-route.post("/add-product",async(req,res)=>{
-    
-})
+route.post("/add-product",Auth, async (req, res) => {
+try{
+  const user = await jwt.verify(req.headers.authorization.split(" ")[1],"jsonkey")
+  console.log(user);
+  
+}
+catch(err){
 
-
+}
+});
 
 module.exports = route;
