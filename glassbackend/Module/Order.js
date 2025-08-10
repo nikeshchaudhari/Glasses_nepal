7e24ea7abc1b965dd86a227712e56447c5b6423f
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-10 16:16:38 +0545

    backend

--- glassbackend/Module/Order.js
+++ glassbackend/Module/Order.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 const OrderSchema = new mongoose(
   {
-    fullName: { type: String, require: true },
+   userInfo:{
+     fullName: { type: String, require: true },
     email: { type: String, require: true },
     phone: { type: Number, require: true },
     address: { type: String, require: true },
+
+   },
+   products:{
+      productId:{type:mongoose.Types.ObjectId,ref:"Product"},
+      name:String,
+      price:Number,
+      quantity:Number
+   },
     paymentMethos: {
-      type: mongoose.Types.ObjectId,
-      ref: "payment",
-      require: true,
     },
+    totalAmount:{type:String,require:true},
+    orderDate:{type:Date,default:Date.now},
+    status:{type:String,default:"Pending"}
   },
   { timestamps: true }
 );
