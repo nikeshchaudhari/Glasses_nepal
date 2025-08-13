493b3c62809d4c2504fc5fa189179455a595d117
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-14 01:05:14 +0545

    frontend

--- glassnepal/src/components/SearchInput.jsx
+++ glassnepal/src/components/SearchInput.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const [search, setSearch] = useState("");
-  const [product, setProduct] = useState([]);
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-      const res=  await axios.get("http://localhost:4080/product/all-product");
-        setProduct(res.data.allProduct);
-      } catch (err) {
-        console.log("Something Error...");
-      }
-    };
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//       const res=  await axios.get("http://localhost:4080/product/all-product");
+//         setProduct(res.data.allProduct);
+//       } catch (err) {
+//         console.log("Something Error...");
+//       }
+//     };
+//     fetchData();
+//   }, []);
 
- const filterData = product.filter((item) =>
-    item.name?.toLowerCase().includes(search.toLowerCase())
-  );
+//  const filterData = product.filter((item) =>
+//     item.name?.toLowerCase().includes(search.toLowerCase())
+//   );
 
+const handelData = (e)=>{
+  if(e.key==="Enter"){
+    e.preventDefault();
+    navigate(`/search?query=${search}`)
+  }
+
+}
   return (
     <div className="h-[100px] relative  flex-col items-center justify-center">
       <div className="relative w-full md:w-[700px]">
@@ -31,14 +39,11 @@ const SearchInput = () => {
           className="md:w-full p-3 pl-12 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-2 focus:ring-blue-400"
           value={search}
           onChange={(e)=>setSearch(e.target.value)}
+         onKeyDown={handelData}
         />
         <SearchIcon className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400" />
       </div>
-        <ul>
-        {filterData.map((item) => (
-          <li key={item._id}>{item.name}</li>
-        ))}
-      </ul>
+      
     </div>
   );
 };
