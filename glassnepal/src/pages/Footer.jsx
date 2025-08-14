8e065161c05bfccd6bdbb6630995fd8ab2faae45
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-14 16:41:01 +0545

    frontend

--- glassnepal/src/pages/Footer.jsx
+++ glassnepal/src/pages/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
     <>
       <div className="w-screen md:w-full bg-black h-40 relative ">
         <div>
-          <div>
-            <h1 className="md:w-[250px]   text-[30px] font-semibold  rounded-lg p-2 text-white">
+          <div className="w-full ">
+            <h1 className="md:w-[250px] text-[30px] font-semibold  rounded-lg p-2 text-white mx-auto md:mx-auto  ">
               GLASSES NEPAL
             </h1>
           </div>
         </div>
         <div>
-          <hr className="text-white" />
-          <p className="text-white text-center absolute bottom-0 lg:left-130 md:left-100 p-5">
+          <hr className="text-white/20 mt-10" />
+          <p className="text-white text-center absolute bottom-0  left-30 lg:left-130 md:left-100 p-5">
             © 2025 Glasses Nepal. All rights reserved.
           </p>
         </div>
